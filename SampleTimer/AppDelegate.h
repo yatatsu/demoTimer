e45e6a11b2ff195909a7051b6a68e20f8a56d7f2
kitagawa <kitagawa@appirits.com>
2014-07-29 17:22:57 +0900

    Initial Commit

---
+++ SampleTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleTimer
+//
+//  Created by kitagawa on 2014/07/29.
+//  Copyright (c) 2014年 appirits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleTimer
+//
+//  Created by kitagawa on 2014/07/29.
+//  Copyright (c) 2014年 appirits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
